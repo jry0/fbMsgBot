ce358cd605b5210e7ae5aa7d5554fd9a47245e52
jry0 <jerryxing@live.ca>
2020-05-01 14:28:37 -0600

    -Improved user search (added confirmation) -Added messaging to user thread upon confirmation

--- fbMsgBot.py
+++ fbMsgBot.py
@@ -1,23 +1,35 @@
 from fbchat import Client
-
+from fbchat.models import *
+#from getpass import getpass
 # Get login credentials from file
 fileLogin = open("C:/Users/JerryX/Desktop/fbChatLogin.txt", "r")
 userEmail = str(fileLogin.readline())
-userPwd = str(fileLogin.readline())
+userPwd =   str(fileLogin.readline())
 fileLogin.close()
 print("Email: ", userEmail)
 print("Password: ",userPwd)
 
 # Attempt login
 clientBarry = Client(userEmail, userPwd, max_tries = 1)
-sessionBarry = clientBarry.getSession()
-print(sessionBarry)
+print(clientBarry.getSession())
 
-
+# Search for user
 searchName = input("Input name to search: ")
-searchedUserList = clientBarry.searchForUsers("Jerry Xing", limit = 5)
+searchedUserList = clientBarry.searchForUsers(searchName, limit = 5)
+for searchedUser in searchedUserList:
+    # Print User Info
+    print("ID: ", searchedUser.uid)
+    print("Photo: ", searchedUser.photo)
+    print("Is Friend: ", searchedUser.is_friend)
 
-print("ID: ", searchedUserList[0].uid)
+    # Confirm User 
+    if(input("Correct User? (Y/N): ").lower() == "y"):
+        clientBarry.send(Message(text="test"), thread_id=searchedUser.uid, thread_type = ThreadType.USER)
+        break
 
-
-clientBarry.logout()
+# Logout
+try:
+    clientBarry.logout()
+    print("User logged out.")
+except:
+    print("Error logging out.")
